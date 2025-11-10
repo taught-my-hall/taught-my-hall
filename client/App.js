80934b45b4950d41515e5389f76b42df9d6ce3f4
angrypigs <angrypigs.tnt@gmail.com>
2025-11-10 12:27:44 +0100

    cleanup

--- client/App.js
+++ client/App.js
@@ -1,11 +1,12 @@
 import { React } from 'react';
 import { Text, View } from 'react-native';
-import 'nativewind';
+
+import stylesMain from './src/styles/stylesMain';
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-blue-500">
-      <Text className="text-2xl font-bold text-white">Hello NativeWind!</Text>
+    <View style={stylesMain.container}>
+      <Text style={stylesMain.text}>Hello</Text>
     </View>
   );
 }
