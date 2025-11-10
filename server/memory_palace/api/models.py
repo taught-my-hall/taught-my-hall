678249d275953ad50787d0ee4952a968c5937315
Anna <annkol11@st.amu.edu.pl>
2025-11-10 01:01:31 +0100

    SCRUM-30 Setup PostgreSQL database, connect to Django and run initial migrations

--- server/memory_palace/api/models.py
+++ server/memory_palace/api/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+
+class Room(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='rooms')
+    name = models.CharField(max_length=100, unique=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return f"{self.name} (user: {self.user.username})"
+
+
+class Furniture(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='furniture')
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    questions = ArrayField(
+        models.CharField(max_length=255),
+        blank=True,
+        default=list,
+        null=False
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name} in room: {self.room.name}"
