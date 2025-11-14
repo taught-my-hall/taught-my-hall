58407429b1891e9cbb1380c9b2d2a728c617ba16
angrypigs <angrypigs.tnt@gmail.com>
2025-11-14 17:57:52 +0100

    Backrooms page with dummy data

--- client/App.js
+++ client/App.js
@@ -3,6 +3,7 @@ import { createStaticNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './src/screens/HomeScreen';
 import LoginScreen from './src/screens/LoginScreen';
+import BackroomScreen from './src/screens/BackroomScreen';
 
 const RootStack = createNativeStackNavigator({
   screens: {
@@ -12,6 +13,9 @@ const RootStack = createNativeStackNavigator({
     },
     Login: {
       screen: LoginScreen,
+    },
+    Backrooms: {
+      screen: BackroomScreen,
     },
   },
 });
