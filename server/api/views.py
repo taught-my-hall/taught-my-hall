79a4e94b00fe7005c4ca4b02bca9c5e26c21ae7a
annkols <kolanos.anna@gmail.com>
2025-11-17 20:37:26 +0100

    Implement CRUD endpoints for Rooms

--- server/api/views.py
+++ server/api/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from .models import Room
+import json
 
 def hello(request):
     return JsonResponse({'message': 'Hello world!'})
@@ -9,14 +11,43 @@ def foobar(request):
     return JsonResponse({'message': "Foobar endpoint"})
 
 def rooms_list(request):
-    rooms = Room.objects.all()
+    """
+    GET = return list of rooms
+    POST = create new room
+    """
+    if request.method == "GET":
+        rooms = Room.objects.all()
+        data = [
+            {
+                "id": room.id,
+                "name": room.name
+            }
+            for room in rooms
+        ]
+        return JsonResponse(data, safe=False)
 
-    data = [
-        {
-            "id": room.id,
-            "name": room.name
-        }
-        for room in rooms
-    ]
+    if request.method == "POST":
+        body = json.loads(request.body)
+        room = Room.objects.create(name=body["name"])
+        return JsonResponse({"id": room.id, "name": room.name}, status=201)
+        
+@csrf_exempt
+def room_detail(request, room_id):
+    """
+    PUT = update
+    DELETE = delete
+    """
+    try:
+        room = Room.objects.get(id=room_id)
+    except Room.DoesNotExist:
+        return JsonResponse({"error": "Room not found"}, status=404)
 
-    return JsonResponse(data, safe=False)
+    if request.method == "PUT":
+        body = json.loads(request.body)
+        room.name = body.get("name", room.name)
+        room.save()
+        return JsonResponse({"id": room.id, "name": room.name})
+
+    if request.method == "DELETE":
+        room.delete()
+        return JsonResponse({"status": "deleted"})
