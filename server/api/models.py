516cd5ef1d7c693faa6f90aef72c53aeb10112c9
Kornel <kornelszkutnik@gmail.com>
2025-12-04 12:17:01 +0100

    Fix DB relations and update seed data & views accordingly

--- server/api/models.py
+++ server/api/models.py
@@ -5,20 +5,21 @@ from django.utils import timezone
 
 class Room(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='rooms')
+
     name = models.CharField(max_length=100, unique=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
 
     def __str__(self):
         return f"{self.name} (user: {self.user.username})"
 
 
 class Furniture(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="furniture")
     room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='furniture')
+
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
-    flashcards = models.JSONField(default=list, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -27,6 +28,9 @@ class Furniture(models.Model):
 
 
 class Flashcard(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="flashcards")
+    furniture = models.ForeignKey(Furniture, on_delete=models.CASCADE, related_name='flashcards')
+
     front = models.TextField()
     back = models.TextField()
     interval = models.IntegerField(default=1)
