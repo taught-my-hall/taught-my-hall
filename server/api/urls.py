c0d6f01a92e9b93ff75f68113acb1687719db023
annkols <kolanos.anna@gmail.com>
2025-12-08 17:11:42 +0100

    TMH-88 Respond only with allowed data

--- server/api/urls.py
+++ server/api/urls.py
@@ -2,8 +2,8 @@ from rest_framework.routers import DefaultRouter
 from .views import RoomViewSet, FurnitureViewSet, FlashcardViewSet
 
 router = DefaultRouter()
-router.register(r"rooms", RoomViewSet)
-router.register(r"furniture", FurnitureViewSet)
+router.register(r"rooms", RoomViewSet, basename="rooms")
+router.register(r"furniture", FurnitureViewSet, basename="furniture")
 router.register(r"flashcards", FlashcardViewSet, basename="flashcards")
 
 urlpatterns = router.urls
